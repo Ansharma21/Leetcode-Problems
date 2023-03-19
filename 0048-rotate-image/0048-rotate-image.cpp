e3b52d78f6b8d2a435e09dfa0c8f900ac958ba78
Ansh Sharma <107696391+Ansharma21@users.noreply.github.com>
2023-03-19 12:40:09 +0530

    Time: 5 ms (30.40%), Space: 7.2 MB (5.56%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        ios_base::sync_with_stdio(false);
+        int r = matrix.size() ;
+        for(int i = 0 ; i < r ; i++){
+            for(int j = 0 ; j < i ; j++){
+                swap(matrix[i][j] , matrix[j][i]) ;
+            }
+        }
+        for(int i = 0; i < r ; i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+        
+    }
+};
