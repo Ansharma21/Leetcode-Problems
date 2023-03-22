6c2b6a67cab9363c8ce92628ef33621497fb7134
Ansh Sharma <107696391+Ansharma21@users.noreply.github.com>
2023-03-22 23:05:48 +0530

    Time: 480 ms (16.63%), Space: 164.2 MB (11.12%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-void combine_krde(int ind,vector<int>output,vector<vector<int>>&v,int n,int k){
+void combines(int ind,vector<int>output,vector<vector<int>>&v,int n,int k){
        if(output.size() == k){
           v.push_back(output);   
           return ;
         }
         for(int i = ind ; i<=n ; i++){
             output.push_back(i);
-            combine_krde(i+1 , output , v , n , k);
+            combines(i+1 , output , v , n , k);
             output.pop_back();
         }
     }
@@ -15,7 +15,7 @@ void combine_krde(int ind,vector<int>output,vector<vector<int>>&v,int n,int k){
         vector<vector<int>>v ;
         int ind = 1 ;
         vector <int> output ;
-        combine_krde(ind , output , v , n , k);
+        combines(ind , output , v , n , k);
         return v ;
     }
 };
