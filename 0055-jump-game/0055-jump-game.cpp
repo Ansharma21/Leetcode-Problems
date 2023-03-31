796a55b48125adccf569ef9325e2cb2a01b2c88f
Ansh Sharma <107696391+Ansharma21@users.noreply.github.com>
2023-03-31 19:06:04 +0530

    Time: 48 ms (99.18%), Space: 48.6 MB (26.40%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
         int dp[nums.size()];
         dp[0] = nums[0] ;
         for(int i = 1 ; i<nums.size() ; i++){
